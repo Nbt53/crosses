de4f3abd870470f3cb6dfa45b443d08c4db6fcec
nbt53 <nbt853.nh@gmail.com>
2023-03-20 22:44:06 +0000

    0.1

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
 const boardContainer = document.querySelector('.board-container');
 const newGameButton = document.querySelector('.newGame');
 
-let board = [[], [], []];
+let x = [];
+let o = [];
+const wins = [
+    ['0-0', '0-1', '0-2'], ['1-0', '1-1', '1-2'], ['2-0', '2-1', '2-2'],
+    ['0-0', '1-0', '2-0'], ['0-1', '1-1', '2-1'], ['0-2', '1-2', '2-2'],
+    ['0-0', '1-1', '2-2'], ['0-2', '1-1', '2-0']
+]
+
 let turn = 0;
-
-game = () => {
-
-
-}
-
 
 drawBoard = () => {
     boardContainer.innerHTML = `<div class="board">
         <div class="row">
-            <div class="col" id="0-0"></div>
-            <div class="col" id="0-1"></div>
-            <div class="col" id="0-2"></div>
+            <div class="col" id="0-0"> </div>
+            <div class="col" id="0-1"> </div>
+            <div class="col" id="0-2"> </div>
         </div>
         <div class="row">
-            <div class="col" id="1-0"></div>
-            <div class="col" id="1-1"></div>
-            <div class="col" id="1-2"></div>
+            <div class="col" id="1-0"> </div>
+            <div class="col" id="1-1"> </div>
+            <div class="col" id="1-2"> </div>
         </div>
         <div class="row">
             <div class="col" id="2-0"></div>
@@ -29,9 +30,9 @@ drawBoard = () => {
         </div>
     </div>`;
     for (let r = 0; r < 3; r++) {
-        document.getElementById(`${r}-0`).addEventListener('click', placeSymbol)
-        document.getElementById(`${r}-1`).addEventListener('click', placeSymbol)
-        document.getElementById(`${r}-2`).addEventListener('click', placeSymbol)
+        document.getElementById(`${r}-0`).addEventListener('click', placeSymbol);
+        document.getElementById(`${r}-1`).addEventListener('click', placeSymbol);
+        document.getElementById(`${r}-2`).addEventListener('click', placeSymbol);
     }
 
 }
@@ -41,15 +42,50 @@ placeSymbol = (e) => {
         const selected = e.target.id
         const square = document.getElementById(selected)
         square.innerHTML = 'O';
+        o.push(selected)
+        square.removeEventListener('click', placeSymbol)
+        checkForWin(o)
         turn++;
     } else if (turn === 1) {
         const selected = e.target.id
         const square = document.getElementById(selected)
         square.innerHTML = 'X';
+        x.push(selected)
+        square.removeEventListener('click', placeSymbol)
+        checkForWin(x)
         turn--;
-    } else {
-        alert('Game is fucked start a new one');
     }
+}
+
+checkForWin = (player) => {
+
+    for (i = 0; i < wins.length; i++) {
+        let toCheck = wins[i]
+        let matches = 0
+        for (j = 0; j < player.length; j++) {
+
+            if (player[j] === toCheck[0]) {
+                matches++
+            }
+            if (player[j] === toCheck[1]) {
+                matches++
+            }
+            if (player[j] === toCheck[2]) {
+                matches++
+            }
+        }
+        if (matches === 3) {
+            alert(`We Have a winner`)
+            reset()
+        }
+    }
+}
+
+reset = () => {
+    x = []
+    o = []
+    turn = 0
+    boardContainer.innerHTML = ''
 }
 
 newGameButton.addEventListener('click', drawBoard);
