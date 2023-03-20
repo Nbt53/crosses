5373a092ae577a0cd794178503bbfd8610004c89
nbt53 <nbt853.nh@gmail.com>
2023-03-20 23:03:36 +0000

    0.2

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const wins = [
 let turn = 0;
 
 drawBoard = () => {
+    reset()
     boardContainer.innerHTML = `<div class="board">
         <div class="row">
             <div class="col" id="0-0"> </div>
@@ -30,25 +31,22 @@ drawBoard = () => {
         </div>
     </div>`;
     for (let r = 0; r < 3; r++) {
-        document.getElementById(`${r}-0`).addEventListener('click', placeSymbol);
-        document.getElementById(`${r}-1`).addEventListener('click', placeSymbol);
-        document.getElementById(`${r}-2`).addEventListener('click', placeSymbol);
+        for (let c = 0; c < 3; c++) {
+            document.getElementById(`${r}-${c}`).addEventListener('click', placeSymbol);
+        }
     }
-
 }
 
 placeSymbol = (e) => {
-    if (turn === 0) {
-        const selected = e.target.id
-        const square = document.getElementById(selected)
+    const selected = e.target.id
+    const square = document.getElementById(selected)
+    if (turn === 0) { 
         square.innerHTML = 'O';
         o.push(selected)
         square.removeEventListener('click', placeSymbol)
         checkForWin(o)
         turn++;
     } else if (turn === 1) {
-        const selected = e.target.id
-        const square = document.getElementById(selected)
         square.innerHTML = 'X';
         x.push(selected)
         square.removeEventListener('click', placeSymbol)
@@ -63,20 +61,19 @@ checkForWin = (player) => {
         let toCheck = wins[i]
         let matches = 0
         for (j = 0; j < player.length; j++) {
-
-            if (player[j] === toCheck[0]) {
-                matches++
-            }
-            if (player[j] === toCheck[1]) {
-                matches++
-            }
-            if (player[j] === toCheck[2]) {
-                matches++
+            for (p = 0; p < 3; p++) {
+                if (player[j] === toCheck[p]) {
+                    matches++
+                }
             }
         }
         if (matches === 3) {
             alert(`We Have a winner`)
-            reset()
+            for (let r = 0; r < 3; r++) {
+                for (let c = 0; c < 3; c++) {
+                    document.getElementById(`${r}-${c}`).removeEventListener('click', placeSymbol);
+                }
+            }
         }
     }
 }
@@ -87,5 +84,6 @@ reset = () => {
     turn = 0
     boardContainer.innerHTML = ''
 }
+
 
 newGameButton.addEventListener('click', drawBoard);
